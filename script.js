e4a3d943cff05ed3c225d0cbc033ad54cb2eac77
IURII <yamusagitov@gmail.com>
2019-06-09 11:20:49 +0500

    lesson2

---
+++ script.js
@@ -0,0 +1,17 @@
+let num = 100;
+
+switch (num) {
+    case num > 40:
+        console.log("Много!");
+        break;
+    case num < 40:
+        console.log("Мало");
+        break;
+    case 100:
+        console.log("Верно!");
+        break;
+    default:
+        console.log("что-то пошло не так")
+        break;
+
+}

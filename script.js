46f12ad678537b0c760ffaf32e1749f857f71e02
IURII <yamusagitov@gmail.com>
2019-06-09 11:46:26 +0500

    lesson2_do_while_for

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ switch (num) {
         break;
 
 }
+
+let num = 50
+/*while (num < 55) {
+    console.log(num);
+    num++;
+}*/
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue
+    }
+    console.log(i);
+}
